f5c7ed497e55ebeed4daba60d2c7866d6e7baf78
Denis <denis.tolmachev@gmail.com>
2022-11-14 19:47:48 +0200

    added background end text

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        style={styles.imageBg}
+        source={require("./assets/img/bg-img.jpg")}
+      >
+        <Text style={styles.title}>Hello World!!!</Text>
+        <Text style={styles.text}>My First React Native Project!!!</Text>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +19,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  imageBg: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 38,
+    color: "white",
+  },
+  text: {
+    fontSize: 28,
+    color: "white",
   },
 });
